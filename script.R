c6caf4dd25fbe59ecd03d201ae2ea3eaefd55f71
gustavo-hideo <36019434+gustavo-hideo@users.noreply.github.com>
2021-01-28 09:55:47 -0700

    Update script.R

--- script.R
+++ script.R
@@ -42,7 +42,7 @@ library(BatchGetSymbols)
 library(lubridate)
 
 # -- Getting and using my credentials
-source('C:\wandata\credentials\rhcred.R')
+source('C:/wandata/credentials/rhcred.R')
 RH = RobinHood(username = USERNAME, password = PSSWD)
 
 
